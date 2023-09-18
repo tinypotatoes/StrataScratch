7d003e073f22bf3c81cc4e53f0a4037103859ec5
Sebastian Granhold <102340133+tinypotatoes@users.noreply.github.com>
2023-09-18 14:32:55 -0500

    Update First_Name_Ends_A.sql
    Added information

---
+++ SQL/Easy/First_Name_Ends_A.sql
@@ -0,0 +1,20 @@
+/*
+Find all workers whose first name ends with the letter 'a'.
+https://platform.stratascratch.com/coding/9841-find-all-workers-whose-first-name-ends-with-the-letter-a?code_type=1
+
+Difficulty: Easy
+
+Tables:
+(worker)
+worker_id      int
+first_name     varchar
+last_name      varchar
+salary         int
+joining_date   datetime
+department     varchar
+
+*/
+
+select * 
+from worker
+where worker.first_name LIKE '%a'
